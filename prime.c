7440077137facf0940da2c5543a4fd6f81d8bc3b
chiu-chiu-kuan <devaraja1945@gmail.com>
2022-10-25 22:31:55 +0800

    fix some problem when i<=n

--- prime.c
+++ prime.c
@@ -16,34 +16,36 @@ int main(void)
         prime[i]=true;
     }
     int j=2;// NO.j prime
-    while(j*j<n)
+    while(j*j<=n)
     {
         if(!prime[j])
             j++;//再下一位
         else//是質數，幹掉他的倍數
         {
-            for(int i=2*j;i<n;i+=j)//如果j=0程式會卡在這
+            for(int i=2*j;i<=n;i+=j)//如果j=0程式會卡在這
             {
                 prime[i]=false;
             }
             j++;//下一位
         }
     }
-    int count=-1;
-    for(int i=0;i<n;i++)
+int count=0;
+for(int i=2;i<=n;i++)
+{
+    if(prime[i])
     {
-        if(prime[i])
-        {
-            printf("%d ",i);
-            count++;
+        if(count%column==column-1){
+        printf("%d-\n",i);//最後一個
         }
-        if(count%column==column-1)
-        {
-            printf("\n");
-            count=0;
+        else{
+        printf("%d- ",i);
         }
+        count++;
+        //printf(">>%d ",count);
+        
+        
     }
-    
+}
 
     return 0;
 }
